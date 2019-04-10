80fade51077fbb232d3d9bcbd4e0d2347ec0bff7
AlexDerbenew <alex.derbenew@yandex.ru>
2019-04-10 17:20:23 +0300

    Implemented code for iframe page version

--- chrome_extension/open_link.js
+++ chrome_extension/open_link.js
@@ -1,5 +1,14 @@
 'use strict'
 
-var temp = document.querySelector('[data-cid=UF_CRM_1528388648]');
-var text = temp.querySelector('.crm-entity-widget-content-block-inner').firstChild.firstChild.innerText;
+
+if( document.getElementsByClassName('side-panel-iframe')[0] == undefined ){
+    var temp = document.querySelector('[data-cid=UF_CRM_1528388648]');
+    var text = temp.querySelector('.crm-entity-widget-content-block-inner').firstChild.firstChild.innerText;
+}
+else{
+    var iframe = document.getElementsByClassName('side-panel-iframe')[0];
+    var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+    var text = innerDoc.documentElement.querySelector(['[data-cid=UF_CRM_1528388648]']).querySelector('.crm-entity-widget-content-block-inner').firstChild.firstChild.innerText;
+}
 window.open("crmfile://"+text,"_blank");
+
